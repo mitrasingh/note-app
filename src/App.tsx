b5b35bd08dc98af774ccc284cb1ad6e4f52dab0c
Mitra Singh <mitra.coder@gmail.com>
2024-02-29 23:09:09 -0500

    added container with padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<h1>Home</h1>}></Route>
-			<Route path="/new" element={<h1>New</h1>}></Route>
-			<Route path="/path:id">
-				<Route index element={<h1>Show</h1>} />
-				<Route path="edit" element={<h1>Edit</h1>} />
-			</Route>
-			<Route path="*" element={<Navigate to="/" />}></Route>
-		</Routes>
+		<Container className="py-4">
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>}></Route>
+				<Route path="/new" element={<h1>New</h1>}></Route>
+				<Route path="/path:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" element={<h1>Edit</h1>} />
+				</Route>
+				<Route path="*" element={<Navigate to="/" />}></Route>
+			</Routes>
+		</Container>
 	);
 }
 
