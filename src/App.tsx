b23baacf5876d7ae9ecd3bbe2cd007d97ef8f493
Mitra Singh <mitra.coder@gmail.com>
2024-03-19 23:27:13 -0400

    added: component for /new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import NewNote from "./NewNote";
 
 function App() {
 	return (
 		<Container className="py-4">
 			<Routes>
 				<Route path="/" element={<h1>Home</h1>}></Route>
-				<Route path="/new" element={<h1>New</h1>}></Route>
+				<Route path="/new" element={<NewNote />}></Route>
 				<Route path="/path:id">
 					<Route index element={<h1>Show</h1>} />
 					<Route path="edit" element={<h1>Edit</h1>} />
