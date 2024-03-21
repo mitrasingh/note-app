f0c11eac4c89157025cf018894c19ffa9e3294c6
Mitra Singh <mitra.coder@gmail.com>
2024-03-20 23:37:51 -0400

    setup structure of form fields and buttons

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,5 +1,46 @@
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CreatableReactSelect from "react-select/creatable";
+
 const NoteForm = () => {
-	return <h1>Form</h1>;
+	return (
+		<Form>
+			<Stack gap={4}>
+				<Row>
+					<Col>
+						<Form.Group controlId="title">
+							<Form.Label>Title</Form.Label>
+							<Form.Control required />
+						</Form.Group>
+					</Col>
+					<Col>
+						<Form.Group controlId="tags">
+							<Form.Label>Tags</Form.Label>
+							<CreatableReactSelect isMulti />
+						</Form.Group>
+					</Col>
+				</Row>
+				<Form.Group controlId="markdown">
+					<Form.Label>Body</Form.Label>
+					<Form.Control required as="textarea" rows={15} />
+				</Form.Group>
+			</Stack>
+			<Stack
+				direction="horizontal"
+				gap={2}
+				className="justify-content-end mt-3"
+			>
+				<Button type="submit" variant="primary">
+					Save
+				</Button>
+				<Link to="..">
+					<Button type="button" variant="outline-secondary">
+						Cancel
+					</Button>
+				</Link>
+			</Stack>
+		</Form>
+	);
 };
 
 export default NoteForm;
